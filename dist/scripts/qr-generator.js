b72a260780d3fcb749cd2d53aadddcb64ec5ad97
bhargablinx <bhargabbhuyan001@gmail.com>
2024-12-16 17:32:21 +0530

    completed

--- dist/scripts/qr-generator.js
+++ dist/scripts/qr-generator.js
@@ -4,15 +4,26 @@ function showQR(imgUrl) {
   const container = document.querySelector(".qr-code-area");
   const img = document.createElement("img");
   img.src = imgUrl;
-  img.classList.add("mt-10", "ml-3");
+  img.classList.add("mt-10", "ml-3", "your-qr");
   container.appendChild(img);
 }
 
 function getQR(domain, size) {
-  const modifiedDomain = `https://${domain}`;
-  const URL = `http://api.qrserver.com/v1/create-qr-code/?data=${modifiedDomain}&size=${size}x${size}`;
-  return URL;
+  if (dataType == "url") {
+    const modifiedDomain = `https://${domain}`;
+    const URL = `http://api.qrserver.com/v1/create-qr-code/?data=${modifiedDomain}&size=${size}x${size}`;
+    return URL;
+  } else {
+    const URL = `http://api.qrserver.com/v1/create-qr-code/?data=${domain}&size=${size}x${size}`;
+    return URL;
+  }
 }
+
+function removeOldQR() {
+  document.querySelector(".your-qr").remove();
+}
+
+function convertingValidUrl() {}
 
 document.querySelectorAll(".data-selection-btn").forEach((item) => {
   item.addEventListener("click", (e) => {
@@ -37,6 +48,7 @@ document.querySelectorAll(".data-selection-btn").forEach((item) => {
 });
 
 document.querySelector(".generate-btn").addEventListener("click", (e) => {
+  if (document.querySelector(".your-qr") != null) removeOldQR();
   if (dataType == "url") {
     let url =
       e.target.previousElementSibling.previousElementSibling.childNodes[1]
@@ -45,7 +57,23 @@ document.querySelector(".generate-btn").addEventListener("click", (e) => {
       e.target.previousElementSibling.previousElementSibling.childNodes[3]
         .childNodes[3].value;
     if (size == "") size = "200";
-    const imgUrl = getQR(url, size);
-    showQR(imgUrl);
+    if (url != "") {
+      const imgUrl = getQR(url, size);
+      showQR(imgUrl);
+    } else {
+      alert("Invalid Url");
+    }
+  } else {
+    let textData =
+      e.target.previousElementSibling.childNodes[1].childNodes[3].value;
+    let size =
+      e.target.previousElementSibling.childNodes[3].childNodes[3].value;
+    if (size == "") size = "200";
+    if (textData != "") {
+      const imgUrl = getQR(textData, size);
+      showQR(imgUrl);
+    } else {
+      alert("Invalid Text");
+    }
   }
 });
