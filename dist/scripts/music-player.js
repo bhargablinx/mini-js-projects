3237ae8976805536e8316c6a174554e3dae96227
bhargablinx <bhargabbhuyan001@gmail.com>
2024-12-18 15:50:10 +0530

    completed

--- dist/scripts/music-player.js
+++ dist/scripts/music-player.js
@@ -1,6 +1,18 @@
 const audio = document.querySelector("audio");
 const playBtn = document.querySelector(".play-btn");
 const pauseBtn = document.querySelector(".pause-btn");
+const progressBar = document.querySelector(".progress-bar");
+
+audio.onloadeddata = () => {
+  progressBar.max = audio.duration;
+  progressBar.value = audio.currentTime;
+};
+
+if (audio.play()) {
+  setInterval(() => {
+    progressBar.value = audio.currentTime;
+  }, 500);
+}
 
 playBtn.addEventListener("click", () => {
   audio.play();
@@ -12,4 +24,19 @@ pauseBtn.addEventListener("click", () => {
   audio.pause();
   playBtn.classList.remove("hidden");
   pauseBtn.classList.add("hidden");
+});
+
+progressBar.addEventListener("click", () => {
+  audio.play();
+  audio.currentTime = progressBar.value;
+});
+
+document.querySelector(".fa-backward").addEventListener("click", () => {
+  audio.currentTime -= 5;
+  progressBar.value = audio.currentTime;
+});
+
+document.querySelector(".fa-forward").addEventListener("click", () => {
+  audio.currentTime += 5;
+  progressBar.value = audio.currentTime;
 });
